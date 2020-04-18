2f43e971935de4d669a87d1e046d7136d4267ee3
j-actualized <56951160+j-actualized@users.noreply.github.com>
2020-04-18 21:57:47 +0530

    bug fix

--- src/services/api/post-job-enquiry.js
+++ src/services/api/post-job-enquiry.js
@@ -1,18 +1,18 @@
 
 
-import axios from '../axios/axios'
+import axios from '../axios/axios';
 
 const postJobEnquiry = (callback, data) => {
-  axios.post(`/resources/`, data, {
+  axios.post(`careers/enquiries/`, data, {
       headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
 
-      }
+    }
   })
-      .then((res) => {
-          callback(res)
-      }).catch(err => callback(err))
+    .then((res) => {
+      callback(res)
+    }).catch(err => callback(err))
 }
 
 export default postJobEnquiry
