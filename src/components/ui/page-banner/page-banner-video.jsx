0d097044effc69292704bab9f0c5307780e35717
j-actualized <56951160+j-actualized@users.noreply.github.com>
2021-02-25 16:10:04 +0530

    banner z-index and quick enquiry popup close button and popup

--- src/components/ui/page-banner/page-banner-video.jsx
+++ src/components/ui/page-banner/page-banner-video.jsx
@@ -6,26 +6,28 @@
 import React from 'react';
 
 import './page-banner.css';
-import bannervideo from './assets/images/Dsc 0208-1(1).m4v';
+import bannervideo from './assets/images/Dsc 0208-1(1).mp4';
 import overlay from './assets/images/overlay.png';
 
 const pageBanner = (props) => {
   return (
     <div className={`page-banner--video`}>
-      <div className={`page-banner--video-color-overlay`}></div>
-      <img src={overlay} className={`page-banner--video-overlay`} />
-      <video oncontextmenu="return false;" className='page-banner--video1' autoPlay loop muted playsinline controls={false} controlsList="nodownload">
-        <source src={bannervideo} type="video/mp4" />
-      </video>
 
       <div className="container container--page-banner">
-        <h1 className="primary-heading u-text-center">
+        <h1 className="primary-heading u-text-center popup--title-container">
           <span className="primary-heading--main">{props.heading}</span>
           <hr className="hr--primary-heading" />
           <span className="primary-heading--sub">{props.subHeading}</span>
         </h1>
         {props.children}
       </div>
+
+      <div className={`page-banner--video-color-overlay`}> </div>
+      <img src={overlay} className={`page-banner--video-overlay`} />
+      <video oncontextmenu="return false;" className='page-banner--video1' autoPlay loop muted playsinline controls={false} controlsList="nodownload">
+        <source src={bannervideo} type="video/mp4" />
+      </video>
+
     </div>
   );
 };
